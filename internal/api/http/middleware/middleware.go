be31485bdcef4d474346c12f5eaf07c62e734c29
antongoncharik <ant.goncharik@gmail.com>
2025-01-24 21:10:02 +0300

    Remove extra headers for options

--- internal/api/http/middleware/middleware.go
+++ internal/api/http/middleware/middleware.go
@@ -13,9 +13,6 @@ func UseCORS() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
 		if c.Request.Method == "OPTIONS" {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-			c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-			c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 			c.AbortWithStatus(http.StatusNoContent)
 			return
 		}
