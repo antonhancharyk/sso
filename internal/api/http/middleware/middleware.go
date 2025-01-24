a64b16b288dc20ae4db0785ab6080af867f37a05
antongoncharik <ant.goncharik@gmail.com>
2025-01-24 21:03:44 +0300

    Remove certbot

--- internal/api/http/middleware/middleware.go
+++ internal/api/http/middleware/middleware.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,10 +12,10 @@ func UseCORS() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
-		// if c.Request.Method == "OPTIONS" {
-		// 	c.AbortWithStatus(http.StatusNoContent)
-		// 	return
-		// }
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
 
 		c.Next()
 	}
