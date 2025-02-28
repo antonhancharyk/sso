462521dba6a69f995363a9be92a78c036707fb19
antongoncharik <ant.goncharik@gmail.com>
2025-02-28 18:03:56 +0300

    Improve certbot and cicd

--- config/certbot/start_certbot.sh
+++ config/certbot/start_certbot.sh
@@ -4,27 +4,42 @@ trap 'exit 0' TERM
 
 export PATH="/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin"
 
-while :; do
-  cert_path="/etc/letsencrypt/live/ssoauth.online/fullchain.pem"
+cert_path="/etc/letsencrypt/live/ssoauth.online/fullchain.pem"
+domain="ssoauth.online"
+email="ant.goncharik.development@gmail.com"
+webroot="/var/www/certbot"
 
-  if [ ! -f $cert_path ]; then
+while :; do
+  if [ ! -f "$cert_path" ]; then
       echo "Certificate not found. Requesting new certificate."
-      certbot certonly --webroot --webroot-path=/var/www/certbot --email ant.goncharik.development@gmail.com --agree-tos --no-eff-email -d ssoauth.online
+      certbot certonly --webroot --webroot-path="$webroot" --email "$email" --agree-tos --no-eff-email -d "$domain"
+
+      if [ $? -eq 0 ]; then
+          echo "Certificate issued successfully. Reloading nginx."
+          docker exec nginx nginx -s reload
+      else
+          echo "Failed to obtain certificate."
+      fi
   else
       echo "Certificate found. Checking expiry date."
-      expiry_date=$(openssl x509 -enddate -noout -in $cert_path | cut -d= -f2)
+      expiry_date=$(openssl x509 -enddate -noout -in "$cert_path" | cut -d= -f2)
       expiry_seconds=$(date -d "$expiry_date" +%s)
       current_seconds=$(date +%s)
       diff_seconds=$((expiry_seconds - current_seconds))
 
-      # If certificate expires in less than 30 days (2592000 seconds), renew it
-      if [ $diff_seconds -le 2592000 ]; then
+      if [ "$diff_seconds" -le 2592000 ]; then
           echo "Certificate is expiring in less than 30 days. Renewing."
-          certbot renew --deploy-hook "/usr/sbin/nginx -s reload" --disable-hook-validation
+          certbot renew --deploy-hook "docker exec nginx nginx -s reload"
+
+          if [ $? -eq 0 ]; then
+              echo "Certificate renewed successfully and nginx reloaded."
+          else
+              echo "Failed to renew certificate."
+          fi
       else
           echo "Certificate is valid for more than 30 days. No renewal needed."
       fi
   fi
 
-  sleep 24h & wait $!
+  sleep 86400
 done
